01426c935cfc7b621706f7ab830bf48d4661df6c
takanokenji-s <takanokenjisms@gmail.com>
2023-02-12 17:34:19 -0300

    Products
    Adding products structure (views and widgets). Consumers for providers added to products view

--- lib/views/product_detail.dart
+++ lib/views/product_detail.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetail extends StatelessWidget {
   static const routeName = '/product-detail';
@@ -8,10 +11,17 @@ class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    /**
+     * listen: false
+     * means if your products changes, it will not listen to this change
+     * so it will not be rebuild
+     */
+    final product =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(product.title),
       ),
     );
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,44 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../views/product_detail.dart';
+import '../providers/product.dart';
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  const ProductItem(this.id, this.title, this.imageUrl, {super.key});
+  const ProductItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
+    /* Provider method forces this Widget to rebuild entirely
+     * if data changes.
+     * use this way if you need everything to be rebuild
+     * reminder: listen false forces product to not listen changes if occurs
+     * */
+    final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
+
+    /**
+     * This method does not forces all widget to rebuild if data changes
+     */
+    return // Consumer<Product>(
+        // builder: (ctx, product, child) =>
+        ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: const Icon(Icons.favorite),
-            color: Theme.of(context).colorScheme.secondary,
-            onPressed: () {},
-          ),
+          /**
+           * By using Consumer like this, it forces
+           * listening changes but in a small part of the
+           * Widget (the icon button in this case)
+           */
+          leading: Consumer<Product>(
+              // use _ instead of child if don't need
+              builder: (ctx, product, child) => IconButton(
+                    // label: child, // a label I would like and should be in child property
+                    icon: Icon(product.isFavorite
+                        ? Icons.favorite
+                        : Icons.favorite_border),
+                    color: Theme.of(context).colorScheme.secondary,
+                    onPressed: () {
+                      product.toggleFavoriteStatus();
+                    },
+                  ) /*, child: Text('something I would like in a label sample') */),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
             icon: const Icon(Icons.shopping_cart),
             color: Theme.of(context).colorScheme.secondary,
-            onPressed: (() {}),
+            onPressed: (() {
+              cart.addItem(product.id, product.price, product.title);
+            }),
           ),
         ),
         child: GestureDetector(
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetail.routeName,
-              arguments: id,
+              arguments: product.id,
             );
           },
-          child: Image.network(imageUrl, fit: BoxFit.cover),
+          child: Image.network(product.imageUrl, fit: BoxFit.cover),
         ),
       ),
     );
