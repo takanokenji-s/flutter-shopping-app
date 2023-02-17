e9ed8ddc3dc35e9a19a2f785157d090637b6cbc2
takanokenji-s <takanokenjisms@gmail.com>
2023-02-16 22:12:09 -0300

    Orders Button Action
    Orders now stores current cart into an order and this order will be listed on orders overview

--- lib/views/cart_view.dart
+++ lib/views/cart_view.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 // show means that only Cart class will be imported without CartItem (which exists in the same file)
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 import '../widgets/cart_item.dart'; //  as cart_item;
 
 class CartView extends StatelessWidget {
@@ -43,7 +44,11 @@ class CartView extends StatelessWidget {
                     foregroundColor:
                         MaterialStateProperty.all<Color>(Colors.purple),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items().values.toList(), cart.totalAmount);
+                    cart.clearCart();
+                  },
                   child: const Text('Order Now'),
                 ),
               ],
@@ -53,11 +58,13 @@ class CartView extends StatelessWidget {
         const SizedBox(
           height: 30,
         ),
+        // Expanded uses all available space inside Column or Row Widgets
         Expanded(
           child: ListView.builder(
             itemCount: cart.items().length,
             itemBuilder: (ctx, i) => CartItem(
               id: cart.items().values.toList()[i].id,
+              productId: cart.items().keys.toList()[i],
               quantity: cart.items().values.toList()[i].quantity,
               price: cart.items().values.toList()[i].price,
               title: cart.items().values.toList()[i].title,
