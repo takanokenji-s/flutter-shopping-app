a035dbdb48399f02ac008af00473ac972a29b667
takanokenji-s <takanokenjisms@gmail.com>
2023-02-19 16:22:50 -0300

    Undo Added Item
    Adding function to remove added item and adding Scaffold message

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -54,6 +54,20 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).colorScheme.secondary,
             onPressed: (() {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Added item to Cart',
+                      textAlign: TextAlign.center),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             }),
           ),
         ),
