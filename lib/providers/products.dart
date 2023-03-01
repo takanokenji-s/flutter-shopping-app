a7c22b1f4278398a5c0d551118048b08114a9dd9
takanokenji-s <takanokenjisms@gmail.com>
2023-02-28 22:59:46 -0300

    Adding Get Request to Products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -2,43 +2,11 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
 import './product.dart';
 
 class Products with ChangeNotifier {
-  final List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    )
-  ];
+  List<Product> _items = [];
 
   // var _filterFavorites = false;
 
@@ -70,23 +38,53 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
+  Future<void> fetchAndSetProducts() async {
+    try {
+      // remove .json from url to force errors
+      const url =
+          'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/products.json'; // .json';
+      final response = await http.get(Uri.parse(url));
+      final decodedResponse =
+          json.decode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      decodedResponse.forEach((productId, product) {
+        loadedProducts.add(Product(
+          id: productId,
+          title: product['title'],
+          description: product['description'],
+          price: product['price'],
+          imageUrl: product['imageUrl'],
+          isFavorite: product['isFavorite'],
+        ));
+      });
+      _items = loadedProducts;
+      notifyListeners();
+    } catch (err) {
+      throw err;
+    }
+  }
+
   // this forces _items to be added inside this method
   // as it forces to add here, notifyListeners can be triggered
-  void addProduct(Product product) {
+  // in this method a async and await was applied, but the .then and .catchError will be comment
+  // remove async and all awaits in order to use with then/catch and set return to http.post...
+  Future<void> addProduct(Product product) async {
+    // remove .json from url to force errors
     const url =
-        'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/products.json';
-    http
-        .post(
-      Uri.parse(url),
-      body: json.encode({
-        'title': product.title,
-        'description': product.description,
-        'price': product.price,
-        'imageUrl': product.imageUrl,
-        'isFavorite': product.isFavorite,
-      }),
-    )
-        .then((response) {
+        'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/products.json'; // .json';
+    // return
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+          'isFavorite': product.isFavorite,
+        }),
+      );
+      // .then((response) {
       final newProduct = Product(
         id: json.decode(response.body)['name'],
         title: product.title,
@@ -94,15 +92,29 @@ class Products with ChangeNotifier {
         price: product.price,
         imageUrl: product.imageUrl,
       );
-      _items.add(newProduct);
       // _items.insert(0, newProduct); // to add as first item in the list
+      _items.add(newProduct);
       notifyListeners();
-    });
+      // }).catchError((err) {
+      //   throw err;
+      // });
+    } catch (err) {
+      throw err;
+    }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final productIndex = _items.indexWhere((element) => element.id == id);
     if (productIndex >= 0) {
+      final url =
+          'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/products/$id.json';
+      await http.patch(Uri.parse(url),
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          }));
       _items[productIndex] = newProduct;
       notifyListeners();
     } else {
@@ -110,8 +122,19 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((element) => element.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url =
+        'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/products/$id.json';
+    final index = _items.indexWhere((element) => element.id == id);
+    Object? currentProduct = _items[index];
+    _items.removeAt(index);
     notifyListeners();
+    final response = await http.delete(Uri.parse(url));
+    if (response.statusCode >= 400) {
+      throw HttpException('Could not delete product');
+    } else {
+      _items.insert(index, currentProduct as Product);
+    }
+    currentProduct = null;
   }
 }
