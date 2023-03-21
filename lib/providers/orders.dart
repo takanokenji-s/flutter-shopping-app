0aee8a2fa2b2550139972c88f67001f8448d5f57
takanokenji-s <takanokenjisms@gmail.com>
2023-03-21 20:43:39 -0300

    Adding endpoint to orders
    Adding orders requests to providers to pick firebase data

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -19,14 +19,18 @@ class OrderItem {
 
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
+  String authToken;
+  String userId;
+
+  Orders(this.authToken, this.userId, this._orders);
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
   Future<void> fetchAndSetOrders() async {
-    const url =
-        'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/orders.json';
+    final url =
+        'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/orders/$userId.json?auth=$authToken';
     final response = await http.get(Uri.parse(url));
     final List<OrderItem> orders = [];
     final encodedOrders = json.decode(response.body) as Map<String, dynamic>;
@@ -55,8 +59,8 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    const url =
-        'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/orders.json';
+    final url =
+        'https://learn-flutter-aa8f6-default-rtdb.firebaseio.com/orders/$userId.json?auth=$authToken';
     final timestamp = DateTime.now();
     final response = await http.post(Uri.parse(url),
         body: json.encode({
