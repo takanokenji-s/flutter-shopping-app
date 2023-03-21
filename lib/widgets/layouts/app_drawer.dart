4f87f2617c2a36ac4c8274944e4d33ca3bc8380f
takanokenji-s <takanokenjisms@gmail.com>
2023-03-21 20:34:09 -0300

    Adding Logout to Drawer
    Adding logout button + method call to clean session

--- lib/widgets/layouts/app_drawer.dart
+++ lib/widgets/layouts/app_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../providers/auth.dart';
 import '../../views/orders_overview.dart';
 import '../../views/user_products_overview.dart';
 
@@ -39,6 +41,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProducts.routeName);
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              Provider.of<Auth>(context, listen: false).logout();
+              Navigator.of(context).pop();
             },
           ),
         ],
