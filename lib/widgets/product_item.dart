0336e534ebd2c9215dea15d2d1c011897b6129b9
takanokenji-s <takanokenjisms@gmail.com>
2023-03-21 20:56:49 -0300

    Adding new resources to views
    All views that uses products are using these new requests now

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,47 +4,32 @@ import 'package:provider/provider.dart';
 import '../views/product_detail.dart';
 import '../providers/product.dart';
 import '../providers/cart.dart';
+import '../providers/auth.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    /* Provider method forces this Widget to rebuild entirely
-     * if data changes.
-     * use this way if you need everything to be rebuild
-     * reminder: listen false forces product to not listen changes if occurs
-     * */
-    final product = Provider.of<Product>(context); // , listen: false
+    final product = Provider.of<Product>(context);
     final cart = Provider.of<Cart>(context, listen: false);
+    final auth = Provider.of<Auth>(context, listen: false);
 
-    /**
-     * This method does not forces all widget to rebuild if data changes
-     */
-    return // Consumer<Product>(
-        // builder: (ctx, product, child) =>
-        ClipRRect(
+    return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          /**
-           * By using Consumer like this, it forces
-           * listening changes but in a small part of the
-           * Widget (the icon button in this case)
-           */
           leading: Consumer<Product>(
-              // use _ instead of child if don't need
-              builder: (ctx, product, child) => IconButton(
-                    // label: child, // a label I would like and should be in child property
-                    icon: Icon(product.isFavorite
-                        ? Icons.favorite
-                        : Icons.favorite_border),
-                    color: Theme.of(context).colorScheme.secondary,
-                    onPressed: () {
-                      product.toggleFavoriteStatus();
-                    },
-                  ) /*, child: Text('something I would like in a label sample') */),
+            builder: (ctx, product, child) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {
+                product.toggleFavoriteStatus(auth.token!, auth.userId!);
+              },
+            ),
+          ),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
